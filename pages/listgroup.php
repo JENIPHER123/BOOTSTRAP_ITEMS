c36ff68174bfe7c626ae7715e11c754038ebe55d
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-22 19:42:13 +0300

    listgroup

--- pages/listgroup.php
+++ pages/listgroup.php
@@ -20,9 +20,164 @@
  <!-- END NAVIGATION SECTION OF PAGE -->
             <div class="body-content p-3">
                   <h1 class='pb-5'>ListGroup</h1>
-                  <p>This project is focused on providing the source codes of bootstrap sippets along with the demo. I also have a recod of 100 projects to do in the course of one year. Each project once completed will either be launched live or uploaded on github for all to access and use as a learning guide.</p>
+                  
+                  <!-- Listgroup example 1 -->
+                  <h1 class='pb-4'>Example 1 !</h1>
+                  <ul class="list-group">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                  </ul>
+                  <!-- Listgroup example 1 ! -->
+
+                  <!-- Listgroup example 2 -->
+                  <br><br>
+                  <h1 class='pb-4'>Example 2 !</h1>
+                  <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                  </ul>
+                  <!-- Listgroup example 2 ! -->
+
+                  <!-- Listgroup example 3 -->
+                  <br><br>
+                  <h1 class='pb-4'>Example 3 !</h1>
+                  <ol class="list-group list-group-numbered">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Cras justo odio</li>
+                  </ol>
+                  <!-- Listgroup example 3 ! -->
+
+                  <!-- Listgroup example 4 -->
+                  <br><br>
+                  <h1 class='pb-4'>Example 4 !</h1>
+                  <ol class="list-group list-group-numbered">
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                      <div class="ms-2 me-auto">
+                        <div class="fw-bold">Subheading</div>
+                        Cras justo odio
+                      </div>
+                      <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                      <div class="ms-2 me-auto">
+                        <div class="fw-bold">Subheading</div>
+                        Cras justo odio
+                      </div>
+                      <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-start">
+                      <div class="ms-2 me-auto">
+                        <div class="fw-bold">Subheading</div>
+                        Cras justo odio
+                      </div>
+                      <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                  </ol>
+                  <!-- Listgroup example 4 ! -->
+
+                  <!-- Listgroup example 5 -->
+                  <br><br>
+                  <h1 class='pb-4'>Example 5 !</h1>
+                  <ul class="list-group list-group-horizontal">
+                    <li class="list-group-item">An item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                  </ul>
+                
+                  <!-- Listgroup example 5 ! -->
+
+                  <!-- Listgroup example 6 -->
+                  <br><br>
+                  <h1 class='pb-4'>Example 6 !</h1>
+                  <ul class="list-group">
+                    <li class="list-group-item">A simple default list group item</li>
+                    <li class="list-group-item list-group-item-primary">A simple primary list group item</li>
+                    <li class="list-group-item list-group-item-secondary">A simple secondary list group item</li>
+                    <li class="list-group-item list-group-item-success">A simple success list group item</li>
+                    <li class="list-group-item list-group-item-danger">A simple danger list group item</li>
+                    <li class="list-group-item list-group-item-warning">A simple warning list group item</li>
+                    <li class="list-group-item list-group-item-info">A simple info list group item</li>
+                    <li class="list-group-item list-group-item-light">A simple light list group item</li>
+                    <li class="list-group-item list-group-item-dark">A simple dark list group item</li>
+                  </ul>
+                  <!-- Listgroup example 6 ! -->
+
+                   <!-- Listgroup example 7 -->
+                   <br><br>
+                   <h1 class='pb-4'>Example 7 !</h1>
+                   <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      A list item
+                      <span class="badge bg-primary rounded-pill">14</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      A second list item
+                      <span class="badge bg-primary rounded-pill">2</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      A third list item
+                      <span class="badge bg-primary rounded-pill">1</span>
+                    </li>
+                  </ul>
+                  <!-- Listgroup example 7 ! -->
+
+                   <!-- Listgroup example 8 -->
+                   <br><br>
+                   <h1 class='pb-4'>Example 8 !</h1>
+                   <ul class="list-group ">
+                      <li class="list-group-item px-5 py-3">
+                        <input class="form-check-input me-1 " type="checkbox" value="" aria-label="...">
+                        First checkbox
+                      </li>
+                      <li class="list-group-item px-5 py-3">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+                        Second checkbox
+                      </li>
+                      <li class="list-group-item px-5 py-3">
+                        <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+                        Fifth checkbox
+                      </li>
+                    </ul>
+                  <!-- Listgroup example 8 ! -->
+
+                   <!-- Listgroup example 9 -->
+                   <br><br>
+                   <h1 class='pb-4'>Example 9 !</h1>
+                   <div class="list-group">
+                      <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        <div class="d-flex w-100 justify-content-between">
+                          <h5 class="mb-1">List group item heading</h5>
+                          <small>3 days ago</small>
+                        </div>
+                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                      </a>
+                      <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex w-100 justify-content-between">
+                          <h5 class="mb-1">List group item heading</h5>
+                          <small class="text-muted">3 days ago</small>
+                        </div>
+                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                        <small class="text-muted">And some muted small print.</small>
+                      </a>
+                      <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex w-100 justify-content-between">
+                          <h5 class="mb-1">List group item heading</h5>
+                          <small class="text-muted">3 days ago</small>
+                        </div>
+                        <p class="mb-1">Some placeholder content in a paragraph.</p>
+                        <small class="text-muted">And some muted small print.</small>
+                      </a>
+                    </div>
+                  <!-- Listgroup example 9 ! -->
             </div>
-            
 
 </div>
 
