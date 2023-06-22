b7c0bdc5307be5c2ffb9a8674012c56a16e07df8
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-22 17:28:28 +0300

    forms

--- pages/forms.php
+++ pages/forms.php
@@ -20,7 +20,66 @@
  <!-- END NAVIGATION SECTION OF PAGE -->
             <div class="body-content p-3">
                   <h1 class='pb-5'>Forms</h1>
-                  <p>This project is focused on providing the source codes of bootstrap sippets along with the demo. I also have a recod of 100 projects to do in the course of one year. Each project once completed will either be launched live or uploaded on github for all to access and use as a learning guide.</p>
+
+
+                 <!-- dropdown example 1 -->
+                 <div class="h3 pb-4">Example 1 !</div>
+                 <form>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label">Email address</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </form>
+                  <!-- dropdown example 1 ! -->
+
+                   <!-- dropdown example 2 -->
+                   <br><br>
+                 <div class="h3 pb-4">Example 2 !</div>
+                 <label for="inputPassword5" class="form-label">Password</label>
+                  <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
+                  <div id="passwordHelpBlock" class="form-text">
+                    Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+                  </div>
+                  <!-- dropdown example 2 ! -->
+
+                <!-- dropdown example 3 -->
+                <br><br><br>
+                 <div class="h3 pb-4">Example 3 disabled !</div>
+                 <form>
+                  <fieldset disabled>
+                    <legend>Disabled fieldset example</legend>
+                    <div class="mb-3">
+                      <label for="disabledTextInput" class="form-label">Disabled input</label>
+                      <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input">
+                    </div>
+                    <div class="mb-3">
+                      <label for="disabledSelect" class="form-label">Disabled select menu</label>
+                      <select id="disabledSelect" class="form-select">
+                        <option>Disabled select</option>
+                      </select>
+                    </div>
+                    <div class="mb-3">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" disabled>
+                        <label class="form-check-label" for="disabledFieldsetCheck">
+                          Can't check this
+                        </label>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </fieldset>
+                </form>
+                <!-- dropdown example 3 ! -->
             </div>
             
 
