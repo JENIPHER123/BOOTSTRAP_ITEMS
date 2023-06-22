0428cbce4b9574668db63dc680a46fe1f172422b
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-22 16:30:19 +0300

    borders

--- pages/borders.php
+++ pages/borders.php
@@ -3,7 +3,9 @@
 <div style="margin-top:-40px;"></div>
 <?php include_once '../partials/sidebar1.php'?> 
 
-  <!-- Page Content Holder -->
+
+
+<!-- Page Content Holder -->
 <div id="content" >
   
 <!-- NAVIGATION SECTION OF PAGE -->
@@ -20,7 +22,45 @@
  <!-- END NAVIGATION SECTION OF PAGE -->
             <div class="body-content p-3">
                   <h1 class='pb-5'>Borders</h1>
-                  <p>This project is focused on providing the source codes of bootstrap sippets along with the demo. I also have a recod of 100 projects to do in the course of one year. Each project once completed will either be launched live or uploaded on github for all to access and use as a learning guide.</p>
+                 <!-- borders example 1 -->
+                <div class="row">
+                  <div class="col-sm-2 m-1 bg-light border"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border-top"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border-right"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border-bottom"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border-left"style="height:150px;"></div>  
+                </div>
+                <br>
+                 <!--  borders example 1 ! -->
+
+                   <!-- borders example 2 -->
+                   <br>
+                <div class="row">
+                  <div class="col-sm-2 m-1 bg-light border border-primary"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-secondary"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-success"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-danger"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-warning"style="height:150px;"></div>  
+                  <div class="col-sm-2 m-1 bg-light border border-white"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-dark"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border-light"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-light border border border-info"style="height:150px;"></div> 
+                </div>
+                <br>
+                 <!--  borders example 2 ! -->
+
+                 <!-- borders example 3 -->
+                <br>
+                <div class="row">
+                  <div class="col-sm-2 m-1 bg-secondary rounded"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-secondary rounded-top"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-secondary rounded-right"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-secondary rounded-bottom"style="height:150px;"></div>
+                  <div class="col-sm-2 m-1 bg-secondary rounded-left"style="height:150px;"></div>  
+                  <div class="col-sm-2 m-1 bg-secondary rounded-circle"style="height:165px;"></div>
+                  <div class="col-sm-2 m-1 bg-secondary rounded-0"style="height:150px;"></div>
+                </div>
+                <!--  borders example 3 ! -->
             </div>
             
 
