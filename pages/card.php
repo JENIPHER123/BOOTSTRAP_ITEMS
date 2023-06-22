0fbf463039ea5b9bad265cc3de4ff38574860b3b
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-22 17:17:36 +0300

    cards

--- pages/card.php
+++ pages/card.php
@@ -19,8 +19,256 @@
             
  <!-- END NAVIGATION SECTION OF PAGE -->
             <div class="body-content p-3">
-                  <h1 class='pb-5'>Card</h1>
-                  <p>This project is focused on providing the source codes of bootstrap sippets along with the demo. I also have a recod of 100 projects to do in the course of one year. Each project once completed will either be launched live or uploaded on github for all to access and use as a learning guide.</p>
+                  <h1 class='pb-5'>Bootstrap Card</h1>
+                  
+                  <!-- card example 1-3 -->
+                  
+                  <div class="row">
+                    <div class="col-sm-4">
+                       <div class="h3 pb-3 pt-5">Example 1 !</div>
+                       <div class="card" style="width: 18rem;">
+                        <img src="../images/img1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                       </div>
+                     </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                    <div class="h3 pb-3 pt-5">Example 2 !</div>
+                     <div class="card" style="width: 18rem;">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                      </div>
+                    </div>
+                    </div>
+
+                    <div class="col-sm-4">
+                    <div class="h3 pb-3 pt-5">Example 3 !</div>
+                    <div class="card" style="width: 18rem;">
+                      <img src="../images/img2.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div>
+                    </div>
+                    </div>
+                  </div>
+                 
+                  <!-- card example 1-3 ! -->
+
+                  <!-- card example 4-6 -->
+                  
+                    <div class="row">
+                       <div class="col-sm-4">
+                       <div class="h3 pb-3 pt-5">Example 4 !</div>
+                         <div class="card" style="width: 18rem;">
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">An item</li>
+                              <li class="list-group-item">A second item</li>
+                              <li class="list-group-item">A third item</li>
+                            </ul>
+                          </div>
+                       </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 5 !</div>
+                        <div class="card" style="width: 18rem;">
+                          <div class="card-header">
+                            Featured
+                          </div>
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">An item</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                          </ul>
+                        </div>
+                      </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 6 !</div>
+                       <div class="card" style="width: 18rem;">
+                          <ul class="list-group list-group-flush">
+                            <li class="list-group-item">An item</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                          </ul>
+                          <div class="card-footer">
+                            Card footer
+                          </div>
+                        </div>   
+                      </div>
+                  </div>
+                 
+              <!-- card example 4-6 ! -->
+
+                  <!-- card example 7-9 -->
+                  
+                      <div class="row">
+                       <div class="col-sm-4">
+                       <div class="h3 pb-3 pt-5">Example 7 !</div>
+                         <div class="card" style="width: 18rem;">
+                            <img src="../images/img4.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                              <li class="list-group-item">An item</li>
+                              <li class="list-group-item">A second item</li>
+                              <li class="list-group-item">A third item</li>
+                            </ul>
+                            <div class="card-body">
+                              <a href="#" class="card-link">Card link</a>
+                              <a href="#" class="card-link">Another link</a>
+                            </div>
+                          </div>
+                       </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 8 !</div>
+                      <div class="card">
+                        <div class="card-header">
+                          Featured
+                        </div>
+                        <div class="card-body">
+                          <h5 class="card-title">Special title treatment</h5>
+                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                      </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 9 !</div>
+                       <div class="card">
+                        <h5 class="card-header">Featured</h5>
+                        <div class="card-body">
+                          <h5 class="card-title">Special title treatment</h5>
+                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                      </div>
+                  </div>   
+
+              <!-- card example 7-9 ! -->
+
+
+              <!-- card example 10-12 -->
+                  
+                    <div class="row">
+                       <div class="col-sm-4">
+                       <div class="h3 pb-3 pt-5">Example 10 !</div>
+                        <div class="card text-center">
+                          <div class="card-header">
+                            Featured
+                          </div>
+                          <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                          <div class="card-footer text-muted">
+                            2 days ago
+                          </div>
+                        </div>
+                       </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 11 !</div>
+                       <div class="card w-75">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                          <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                      </div>
+
+                      <div class="card w-50">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                          <a href="#" class="btn btn-primary">Button</a>
+                        </div>
+                      </div>
+                      </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 12 !</div>
+                       <div class="card mb-3">
+                          <img src="../images/img1.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                          </div>
+                        </div>
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                          </div>
+                          <img src="../images/img1.jpg" class="card-img-bottom" alt="...">
+                        </div>
+                      </div>
+                  </div>   
+              <!-- card example 7-9 ! -->
+
+
+              <!-- card example 10-12 -->
+                  
+                    <div class="row">
+                       <div class="col-sm-4">
+                       <div class="h3 pb-3 pt-5">Example 13 !</div>
+                        <div class="card bg-dark text-white">
+                            <img src="../images/img.jpg" class="card-img" alt="..." >
+                            <div class="card-img-overlay">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                              <p class="card-text">Last updated 3 mins ago</p>
+                            </div>
+                          </div>
+                       </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 14 !</div>
+                       <div class="card mb-3" style="max-width: 540px;">
+                          <div class="row g-0">
+                            <div class="col-md-4">
+                              <img src="../images/img2.jpg" class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                              <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>  
+                      </div>
+
+                      <div class="col-sm-4">
+                      <div class="h3 pb-3 pt-5">Example 15 !</div>
+                      <div class="card border-success mb-3" style="max-width: 18rem;">
+                          <div class="card-header bg-transparent border-success">Header</div>
+                          <div class="card-body text-success">
+                            <h5 class="card-title">Success card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          </div>
+                          <div class="card-footer bg-transparent border-success">Footer</div>
+                        </div>
+                      </div>
+                  </div>   
+              <!-- card example 7-9 ! -->
+            </div>
             </div>
             
 
