c7a7002dfe07959a91e670379050f516c07a4263
JENIPHER123 <jeniperkuki@gmail.com>
2023-06-23 14:51:40 +0300

    popovers

--- pages/popovers.php
+++ pages/popovers.php
@@ -19,8 +19,49 @@
             
  <!-- END NAVIGATION SECTION OF PAGE -->
             <div class="body-content p-3">
-                  <h1 class='pb-5'>Popovers</h1>
-                  <p>This project is focused on providing the source codes of bootstrap sippets along with the demo. I also have a recod of 100 projects to do in the course of one year. Each project once completed will either be launched live or uploaded on github for all to access and use as a learning guide.</p>
+                  <h1 class='pb-5'>Bootstrap Popovers</h1>
+                  
+                  <div class="row">
+                    <div class="col-sm-4">
+                        <!-- popover example 1 -->
+                        <h1 class='pb-4'>Example 1 !</h1>
+                        <button type="button" class="btn btn-secondary m-3" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
+                            Popover on top
+                          </button>
+                          <button type="button" class="btn btn-secondary m-3" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Right popover">
+                            Popover on right
+                          </button>
+                          <button type="button" class="btn btn-secondary m-3" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover">
+                            Popover on bottom
+                          </button>
+                          <button type="button" class="btn btn-secondary m-3" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Left popover">
+                            Popover on left
+                          </button>
+                          <br><br><br>
+                        <!-- popover example 1 ! -->
+                    </div>
+                    <div class="col-sm-4">
+                          <!-- popover example 2 -->
+                          <h1 class='pb-4'>Example 2 !</h1>
+                          <button type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover" title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+                          <br><br><br>
+                          <!-- popover example 2 ! -->
+                    </div>
+                    <div class="col-sm-4">
+                       <!-- popover example 3 -->
+                        <h1 class='pb-4'>Example 3 !</h1>
+                        <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-trigger="focus" title="Dismissible popover" data-bs-content="And here's some amazing content. It's very engaging. Right?">Dismissible popover</a>
+                        <br><br><br>
+                        <!-- popover example 3 ! -->
+                    </div>
+                  </div>
+
+
+               
+
+                  
+
+                   
             </div>
             
 
--- partials/footer.php
+++ partials/footer.php
@@ -13,15 +13,31 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-
+    
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script><!-- useful in popovers -->
+    <script src="../js/bootstrap.bundle.min.js"></script><!-- useful in popovers -->
     <script type="text/javascript">
+
         $(document).ready(function () {
             $('#sidebarCollapse').on('click', function () {
                 $('#sidebar').toggleClass('active');
                 $(this).toggleClass('active');
             });
         });
+    </script>
+
+    <!-- script for to initialize popovers -->
+    <script>
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+            var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+            var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+            })
+
     </script>
 
 
